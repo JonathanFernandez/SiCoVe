1a53126441be7696b7d207fb8acbac141853736a
JonathanFernandez <jonathanerik.fernandez@gmail.com>
2018-10-27 16:49:59 -0300

    entity framework

--- SiCoVe/Site/VerificacionDatos.aspx.cs
+++ SiCoVe/Site/VerificacionDatos.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using DataBaseSiCoVe;
 namespace SiCoVe.Site
 {
     public partial class VerificacionDatos : System.Web.UI.Page
     {
+        sicoveEntities sicove = new sicoveEntities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
