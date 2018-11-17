ac12efb953b3a31460ea45172970dd0fcd559535
enaval94 <44276793+enaval94@users.noreply.github.com>
2018-11-17 20:44:36 -0300

    ListarRemolque baja

--- SiCoVe/Site/ListarRemolque.aspx.cs
+++ SiCoVe/Site/ListarRemolque.aspx.cs
@@ -39,5 +39,46 @@ namespace SiCoVe
         {
 
         }
-    }
+
+        protected void ListadoRemolque()
+        {
+            GvRemolque.DataSource = sicove.SP_LISTADO_PERSONAL_REMOLQUE(null, null, null).ToList();
+            GvRemolque.DataBind();
+        }
+        protected void GvRemolque_delete(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = (int)GvRemolque.DataKeys[e.RowIndex].Values["id"];
+
+            persona per = (from p in sicove.personas where p.id == id select p).FirstOrDefault();
+            //borro los usuarios relacionados a la persona
+            foreach (var u in sicove.usuarios.Where(u => u.persona_id == per.id))
+            {
+                //borro los agentes de transito relacionados a la persona
+                foreach (var a in sicove.personal_remolque.Where(a => a.usuario_id == u.id))
+                {
+                    sicove.personal_remolque.Remove(a);
+                }
+                sicove.usuarios.Remove(u);
+
+            }
+
+            sicove.personas.Remove(per);
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "alertIns", "alert('Usuario eliminado con éxito');", true);
+            sicove.SaveChanges();
+
+
+            ListadoRemolque();
+        }
+
+        protected void GvRemolque_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+
+                int index = Convert.ToInt32(e.CommandArgument);
+                int id = Convert.ToInt32(GvRemolque.DataKeys[index].Value);
+
+            }
+        }
+}
 }
