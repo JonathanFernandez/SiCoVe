2c92c296538e2cc0c3ed225dbf18c4b489c91dfb
enaval94 <44276793+enaval94@users.noreply.github.com>
2018-11-17 15:02:22 -0300

    Alta autoridad de transito

--- SiCoVe/Site/AltaAutoridad.aspx.cs
+++ SiCoVe/Site/AltaAutoridad.aspx.cs
@@ -4,14 +4,138 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataBaseSiCoVe;
 
 namespace SiCoVe
 {
     public partial class AltaAutoridad : SiCoVeMaster
     {
+        sicoveEntities sicove = new sicoveEntities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                CargarCombos();
+            }
 
+        }
+
+        private void CargarCombos()
+        {
+            CargarSexo();
+            CargarNacionalidad();
+            CargarProvincia();
+            CargarLocalidad();
+        }
+
+        private void CargarSexo()
+        {
+
+            var sexo = sicove.sexoes.ToList();
+
+            ddlSexo.DataTextField = "descripcion";
+            ddlSexo.DataValueField = "id";
+            ddlSexo.DataSource = sexo;
+            ddlSexo.DataBind();
+        }
+
+        private void CargarNacionalidad()
+        {
+            var nacionalidades = sicove.nacionalidads.ToList();
+            ddlNacionalidad.DataTextField = "descripcion";
+            ddlNacionalidad.DataValueField = "id";
+            ddlNacionalidad.DataSource = nacionalidades;
+            ddlNacionalidad.DataBind();
+
+        }
+        private void CargarProvincia()
+        {
+            var provincia = sicove.provincias.ToList();
+            ddlProvincia.DataTextField = "descripcion";
+            ddlProvincia.DataValueField = "id";
+            ddlProvincia.DataSource = provincia;
+            ddlProvincia.DataBind();
+        }
+
+        private void CargarLocalidad()
+        {
+            var localidad = sicove.localidads.ToList();
+            ddlLocalidad.DataTextField = "descripcion";
+            ddlLocalidad.DataValueField = "id";
+            ddlLocalidad.DataSource = localidad;
+            ddlLocalidad.DataBind();
+        }
+        protected void btnRegistrarACNC_Click(object sender, EventArgs e)
+        {
+            try
+
+
+            {
+                persona per = new persona();
+                per.apellido = txtApellido.Text;
+                per.nombre = txtNombre.Text;
+                per.dni = Convert.ToInt32(txtNumDocumento.Text);
+                per.sexo_id = Convert.ToInt32(ddlSexo.SelectedValue);
+                per.nacionalidad_id = Convert.ToInt32(ddlNacionalidad.SelectedValue);
+                per.provincia_id = Convert.ToInt32(ddlProvincia.SelectedValue);
+                per.localidad_id = Convert.ToInt32(ddlLocalidad.SelectedValue);
+                per.domicilio = txtDomicilio.Text;
+                per.piso = Convert.ToInt16(txtPiso.Text);
+                per.departamento = txtDepartamento.Text;
+                per.nro_puerta = txtNumPuerta.Text;
+                per.fecha_nacimiento = Convert.ToDateTime(txtFechaNacimiento.Text);
+                per.lugar_nacimiento = txtLugarNacimiento.Text;
+                per.flag_conductor = cckrautoridad.Checked;
+
+                sicove.personas.Add(per);
+
+                usuario usu = new usuario();
+                usu.persona = per;
+                usu.perfil_usuario_id = 3;
+                usu.email = txtEMail.Text;
+                usu.contraseña = txtContraseña.Text;
+
+                sicove.usuarios.Add(usu);
+
+                agente_transito at = new agente_transito();
+                at.usuario = usu;
+                at.nro_legajo = Convert.ToInt32(txtLegajo.Text);
+
+                sicove.agente_transito.Add(at);
+
+
+                sicove.SaveChanges();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alertIns", "alert('Usuario dado de alta con éxito');", true);
+                CleanControl(this.Controls);
+
+            }
+            catch (Exception ex)
+            {
+                LblError.Text = Convert.ToString(ex);
+            }
+        }
+        public void CleanControl(ControlCollection controles)
+        {
+            foreach (Control control in controles)
+            {
+                if (control is TextBox)
+                    ((TextBox)control).Text = string.Empty;
+                else if (control is DropDownList)
+                    ((DropDownList)control).ClearSelection();
+                else if (control is RadioButtonList)
+                    ((RadioButtonList)control).ClearSelection();
+                else if (control is CheckBoxList)
+                    ((CheckBoxList)control).ClearSelection();
+                else if (control is RadioButton)
+                    ((RadioButton)control).Checked = false;
+                else if (control is CheckBox)
+                    ((CheckBox)control).Checked = false;
+                else if (control.HasControls())
+                    //Esta linea detécta un Control que contenga otros Controles
+                    //Así ningún control se quedará sin ser limpiado.
+                    CleanControl(control.Controls);
+            }
         }
     }
 }
