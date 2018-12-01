e6c3b737fe8f0bbdb7f5714f913a6a19ab43e02a
Juan Andres Sobrile <juan_sobrile@hotmail.com>
2018-12-01 17:57:41 -0300

    .

--- SiCoVe/Site/Estadisticas.aspx.cs
+++ SiCoVe/Site/Estadisticas.aspx.cs
@@ -21,10 +21,10 @@ namespace SiCoVe.Site
                 //ddlGraficos.Items.Insert(0, new ListItem("Seleccione una opción...", "0"));
                 ddlGraficos.Items.Insert(0, new ListItem("Controles por Zona", "0"));
                 ddlGraficos.Items.Insert(1, new ListItem("Infracciones por Zona", "1"));
-                //ddlGraficos.Items.Insert(3, new ListItem("Acarreos por Zona", "3"));
-                //ddlGraficos.Items.Insert(4, new ListItem("Denuncias por Zona", "4"));
-                //ddlGraficos.Items.Insert(5, new ListItem("Infracciones por Categoría", "5"));
-                ddlGraficos.SelectedIndex = 1;
+                ddlGraficos.Items.Insert(2, new ListItem("Acarreos por Zona", "3"));
+                ddlGraficos.Items.Insert(3, new ListItem("Denuncias por Zona", "4"));
+                ddlGraficos.Items.Insert(4, new ListItem("Infracciones por Categoría", "5"));
+                ddlGraficos.SelectedIndex = 0;
 
                 /**********************************************************************************************************************/
 
@@ -95,10 +95,74 @@ namespace SiCoVe.Site
         {
             int id_grafico = Convert.ToInt16(ddlGraficos.SelectedValue);
 
-            if (id_grafico == 1)
+            if (id_grafico == 0)
             {
                 lblflot.InnerText = "Controles por Zona";
                 lblbar.InnerText = "Controles por Zona";
+
+                StringBuilder sb;
+
+                int flag = 0;
+                var dataContLoc = sicove.SP_LISTAR_CONTROLES_X_LOCALIDAD().ToList();
+
+                if (dataContLoc.Count > 0)
+                {
+                    sb = new StringBuilder();
+                    sb.Append("var dataMen = [");
+
+                    foreach (SP_LISTAR_CONTROLES_X_LOCALIDAD_Result lo in dataContLoc)
+                    {
+                        if (flag == 0)
+                            sb.Append("{label: '" + lo.LOCALIDAD.ToString() + "', data: " + lo.CANTIDAD + " }");
+                        else
+                            sb.Append(",{label: '" + lo.LOCALIDAD.ToString() + "', data: " + lo.CANTIDAD + " }");
+
+                        flag = 1;
+                    }
+
+                    sb.Append("];");
+
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "Graficos", "\n" + sb.ToString(), true);
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "GraficosPie", "\n llenarFlotPie(dataMen);", true);
+
+                    flag = 0;
+
+                    if (dataContLoc.Count > 0)
+                    {
+                        sb = new StringBuilder();
+                        sb.Append("Morris.Bar({");
+                        sb.Append("element: 'morris-bar-chartAnual',");
+                        sb.Append(" data: [");
+
+                        foreach (SP_LISTAR_CONTROLES_X_LOCALIDAD_Result lo in dataContLoc)
+                        {
+                            if (flag == 0)
+                                sb.Append("{ area: '" + lo.LOCALIDAD.ToString() + "', cantidad: " + lo.CANTIDAD + "}");
+                            else
+                                sb.Append(",{ area: '" + lo.LOCALIDAD.ToString() + "', cantidad: " + lo.CANTIDAD + "}");
+                            flag = 1;
+                        }
+
+                        sb.Append("],");
+
+                        sb.Append("xkey: 'area',");
+                        sb.Append("ykeys: ['cantidad'],");
+                        sb.Append("labels: ['cantidad'],");
+                        sb.Append("barRatio: 0.4,");
+                        sb.Append("xLabelAngle: 35,");
+                        sb.Append("hideHover: 'auto',");
+                        sb.Append("resize: true");
+                        sb.Append("});");
+
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "bAnual", "\n" + sb.ToString(), true);
+                    }
+                }
+            }
+
+            if (id_grafico == 1)
+            {
+                lblflot.InnerText = "Infracciones por Zona";
+                lblbar.InnerText = "Infracciones por Zona";
 
                 StringBuilder sb;
 
