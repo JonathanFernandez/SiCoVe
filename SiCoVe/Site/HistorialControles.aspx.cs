f28f9e6d3b262baa9d62386197d1d39e8820c727
cristian <cristianancutza@gmail.com>
2018-12-01 11:27:34 -0300

    listado de historial de controles

--- SiCoVe/Site/HistorialControles.aspx.cs
+++ SiCoVe/Site/HistorialControles.aspx.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataBaseSiCoVe;
 
 namespace SiCoVe.Site
 {
     public partial class HistorialControles : SiCoVeMaster
     {
+        public sicoveEntities sicove = new sicoveEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!Page.IsPostBack)
+                listadoControles();
+        }
 
+        public void listadoControles()
+        {
+            GvHistorialControles.DataSource = sicove.SP(null, null, null).ToList();
+            GvHistorialControles.DataBind();
         }
     }
 }
