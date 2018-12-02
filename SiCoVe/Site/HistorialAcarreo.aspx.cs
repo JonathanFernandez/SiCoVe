7605f5208aa8504cfffba3dd4edb1eaeee553ac9
cristian <cristianancutza@gmail.com>
2018-12-02 19:29:25 -0300

    Update HistorialAcarreo.aspx.cs

--- SiCoVe/Site/HistorialAcarreo.aspx.cs
+++ SiCoVe/Site/HistorialAcarreo.aspx.cs
@@ -19,16 +19,33 @@ namespace SiCoVe.Site
 
         public void listadoAcarreo()
         {
-            GvHistorialAcarreo.DataSource = sicove.SP_LISTADO_ACARREO(null, null, null).ToList();
-            GvHistorialAcarreo.DataBind();
+            GvHistorialAcarreo.DataSource =
+                (from r in sicove.remolque
+                     join pr in sicove.personal_remolque on r.personal_remolque_id = pr.id
+                     join p in sicove.playa_acarreo on r.playa_acarreo_id = p.id
+                     join v in sicove.vehiculo on r.vehiculo_id = v.id
+                     join l in sicove.localidad on r.localidad_id = l.id
+                 where pr.usuario_id == UserSession.id
+                 select new
+                 {
+                     REMOLCADOR = pr.nro_legajo,
+                     PLAYA = p.descripcion,
+                     FECHA = r.fecha_hora,
+                     UBICACION = r.ubicacion,
+                     DETALLE_INFRACCION = r.detalle_infraccion,
+                     DOMINIO = r.dominio,
+                     LOCALIDAD = l.descripcion
+                 }).ToList();
+
+
         }
 
         protected void btnBuscar_Click(object sender, EventArgs e)
         {
             if(UserSession.perfil_usuario_id != 1)
-                GvHistorialAcarreo.DataSource = sicove.SP_LISTADO_ACARREO(UserSession.persona_id, txtLocalidad.Text, txtDominio.Text).ToList();
+                GvHistorialAcarreo.DataSource = sicove.SP_LISTADO_ACARREO(txtLocalidad.Text, txtDominio.Text).ToList();
             else
-                GvHistorialAcarreo.DataSource = sicove.SP_LISTADO_ACARREO(null, null, null).ToList();
+                GvHistorialAcarreo.DataSource = sicove.SP_LISTADO_ACARREO(null, null).ToList();
             GvHistorialAcarreo.DataBind();
 
         }
